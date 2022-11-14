e1f8ff588c8acf2dc1ab4b3e8ad0fcfc725e9671
Kirill <cs1@list.ru>
2022-11-15 01:41:31 +0300

    setup debug toolbar

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog',
+
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -32,6 +34,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'sensive_blog.urls'
@@ -58,6 +61,10 @@ TEMPLATES = [
     },
 ]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+    
 WSGI_APPLICATION = 'sensive_blog.wsgi.application'
 
 DATABASES = {
