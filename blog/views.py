50f2af71c2eec55aaf0ba610423f763120e9b5a0
Kirill <cs1@list.ru>
2022-11-19 18:54:19 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(posts_count=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -34,7 +34,7 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts_count,
     }
 
 
@@ -88,7 +88,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = post.tags.all()
+    related_tags = post.tags.annotate(posts_count=Count('posts'))
 
     serialized_post = {
         'title': post.title,
