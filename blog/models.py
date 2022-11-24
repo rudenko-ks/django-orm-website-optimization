70dae216267778d75939a1ba9ac8fc8cfa931ff6
Kirill <cs1@list.ru>
2022-11-24 23:07:58 +0300

    fix function name year -> sort_by_year

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.db.models import Count, Prefetch
 
 class PostQuerySet(models.QuerySet):
 
-    def year(self, year):
+    def sort_by_year(self, year):
         posts_at_year = self.filter(
             published_at__year=year).order_by('published_at')
         return posts_at_year
